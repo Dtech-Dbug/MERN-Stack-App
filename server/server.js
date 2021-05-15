9539017b279b4263794e7c019faef05e221d788c
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-05-15 19:19:50 +0530

    updated server.js

--- server/server.js
+++ server/server.js
@@ -7,3 +7,32 @@ require("dotenv").config();
 
 //app
 const app = express();
+
+//DataBase
+mongoose
+	.connect(process.env.MONGO_URI, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useFindAndModify: true,
+	})
+	.then(() => console.log("DB CONNECTED"))
+	.catch((err) => console.log("DB CONNECTION ERR", err));
+
+//middleware
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cors());
+
+//route
+app.get("/", (req, res) => {
+	res.json({
+		data: "wowo, slowly gettin there",
+	});
+});
+
+//port
+const port = env.process.PORT || 8000;
+
+app.listen(port, () => {
+	console.log(`RUNNING ON ${port}`);
+});
