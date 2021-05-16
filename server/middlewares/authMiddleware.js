457c76c94f8a2bbacd541e660aa1bbc81cf6f95b
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-05-16 13:26:47 +0530

    updated middlewares/authMiddleware : next()

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -4,4 +4,7 @@ exports.createOrUpdateUserMiddleware = (req, res, next) => {
 	console.log(req.headers);
 
 	//lets just console.log the request headers: so we will be sending the auth token from FE to BE in headers
+
+	next();
+	//next() : just passes onto the next function
 };
