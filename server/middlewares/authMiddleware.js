1c9f57ecf387f89db3e289bc9e0f8321a81d312f
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-05-16 13:23:29 +0530

    created middlware/authMiddleware

---
+++ server/middlewares/authMiddleware.js
@@ -0,0 +1,7 @@
+const admin = require("../firebase/index");
+
+exports.createOrUpdateUserMiddleware = (req, res, next) => {
+	console.log(req.headers);
+
+	//lets just console.log the request headers: so we will be sending the auth token from FE to BE in headers
+};
