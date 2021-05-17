cfc8225399be07d70eed975059a6ff0a3000c76d
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-05-17 10:16:46 +0530

    update Middlewares/authMidlewares
    Typos in type : string

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,10 +1,24 @@
 const admin = require("../firebase/index");
 
-exports.createOrUpdateUserMiddleware = (req, res, next) => {
-	console.log(req.headers);
-
+exports.createOrUpdateUserMiddleware = async (req, res, next) => {
+	//console.log(req.headers);
 	//lets just console.log the request headers: so we will be sending the auth token from FE to BE in headers
 
-	next();
+	//to validate the token that is posted in the header from the client
+	try {
+		const firebaseUser = await admin
+			.auth()
+			.verifyIdToken(req.headers.authtoken);
+		console.log("FIREBASE USER : ", firebaseUser);
+
+		//we can add more properties in the request and access them from everywrhre , controllers
+		// the req, res are shared through oyt the app ,hence adding request methods will help us access the userToken easily and pass it to the controller , in the routes
+		req.user = firebaseUser;
+
+		next();
+	} catch (err) {
+		alert(err.message);
+	}
+
 	//next() : just passes onto the next function
 };
