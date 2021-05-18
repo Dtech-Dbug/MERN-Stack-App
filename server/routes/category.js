be5dfd031868c38dfed6e75a4b537c88a8c271c9
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-05-18 22:11:38 +0530

    created routes/category.js

---
+++ server/routes/category.js
@@ -0,0 +1,56 @@
+const express = require("express");
+
+const router = express.Router();
+
+//controllers
+const { createOrUpdateUser, currentUser } = require("../controllers/auth");
+
+//middlewares
+const {
+	createOrUpdateUserMiddleware,
+	adminCheckMiddleware,
+} = require("../middlewares/authMiddleware");
+//we need this middleware for auth and admin check
+
+const {
+	create,
+	read,
+	update,
+	remove,
+	list,
+} = require("../controllers/categoryController");
+
+//we do not need to create different end points : we will use same route , but different methods like get,post,put,update
+
+//we do not need middlewares here , we want to diplay all the categiries publically
+router.get("/categories/:slug", list);
+
+router.post(
+	"/category",
+	createOrUpdateUserMiddleware,
+	adminCheckMiddleware,
+	create
+);
+
+//send slug from FE , and based on slug we can query our DB and return the sigle category
+router.get(
+	"/category/:slug",
+	createOrUpdateUserMiddleware,
+	adminCheckMiddleware,
+	read
+);
+
+router.put(
+	"/category/:slug",
+	createOrUpdateUserMiddleware,
+	adminCheckMiddleware,
+	update
+);
+router.delete(
+	"/category/:slug",
+	createOrUpdateUserMiddleware,
+	adminCheckMiddleware,
+	remove
+);
+
+module.exports = router;
