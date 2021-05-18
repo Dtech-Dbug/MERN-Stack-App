2e7c1e32787bc9a44094d09c93b9e0085fdb20f3
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-05-18 22:34:02 +0530

    update categoryController.js

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -1,9 +1,21 @@
 const Category = require("../model/categoryModel");
 
-exports.create = (req, res) => {};
+exports.create = (req, res) => {
+	//
+};
 
-exports.read = (req, res) => {};
+exports.read = (req, res) => {
+	//
+};
 
-exports.update = (req, res) => {};
+exports.update = (req, res) => {
+	//
+};
 
-exports.remove = (req, res) => {};
+exports.remove = (req, res) => {
+	//
+};
+
+exports.list = (req, res) => {
+	//
+};
