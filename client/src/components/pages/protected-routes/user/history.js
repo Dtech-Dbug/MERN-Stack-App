99b00f7c7e14b2ab2c6d1de30831fa4123040f97
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-05-18 18:03:01 +0530

    updated history.js

--- client/src/components/pages/protected-routes/user/history.js
+++ client/src/components/pages/protected-routes/user/history.js
@@ -1,4 +1,5 @@
 import React from "react";
+import UserNav from "../../../Nav/userNav";
 
 //we will have to protect this route , so only logged in users can see this page. if the route is not proteted anyone can navigate to this page without even logging
 
@@ -6,7 +7,10 @@ export const History = () => {
 	return (
 		<div className="container-fluid">
 			<div className="row">
-				<div className="col">WELCOME TO USER History PAGE</div>
+				<div className="col-md-2">
+					<UserNav />
+				</div>
+				<div className="col">user history page</div>
 			</div>
 		</div>
 	);
