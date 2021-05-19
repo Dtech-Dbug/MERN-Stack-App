141b34119fbd3ebe22ab0d11a875bb9a31280392
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-05-19 09:37:15 +0530

    updated categoryController : create function

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -1,18 +1,30 @@
 const Category = require("../model/categoryModel");
 
-exports.create = (req, res) => {
+const slugify = require("slugify");
+
+exports.create = async (req, res) => {
+	//
+	try {
+		//to create catgory : we need data from FE
+		const { name } = req.body;
+
+		//we need name , and slug to slugify the name.We generate slug using slugify
+		const category = await new Cateogty({ name, slug: slugify(name) }).save();
+		res.json(category);
+	} catch (err) {
+		res.status(400).send("Category Creation Failed");
+	}
+};
+
+exports.read = async (req, res) => {
 	//
 };
 
-exports.read = (req, res) => {
+exports.update = async (req, res) => {
 	//
 };
 
-exports.update = (req, res) => {
-	//
-};
-
-exports.remove = (req, res) => {
+exports.remove = async (req, res) => {
 	//
 };
 
