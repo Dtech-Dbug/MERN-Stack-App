476357319842d18e7345aaa618de381c98d5f870
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-05-19 14:01:14 +0530

    updated categoryModel.js

--- server/model/categoryModel.js
+++ server/model/categoryModel.js
@@ -11,7 +11,7 @@ const categorySchema = new mongoose.Schema(
 			type: String,
 			trim: true,
 			required: "Name is required",
-			minlength: [3, "Too Short"],
+			minlength: [2, "Too Short"],
 			maxlength: [32, "Too Long"],
 		},
 		slug: {
