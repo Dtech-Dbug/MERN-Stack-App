7b6ae5be0552024725d8c9229e9ec0cc6359c285
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-05-19 14:04:21 +0530

    update cetgory.js
    While listing and reading category items : no need of middlewares

--- server/routes/category.js
+++ server/routes/category.js
@@ -23,7 +23,7 @@ const {
 //we do not need to create different end points : we will use same route , but different methods like get,post,put,update
 
 //we do not need middlewares here , we want to diplay all the categiries publically
-router.get("/categories/:slug", list);
+router.get("/categories", list);
 
 router.post(
 	"/category",
@@ -35,9 +35,9 @@ router.post(
 //send slug from FE , and based on slug we can query our DB and return the sigle category
 router.get(
 	"/category/:slug",
-	createOrUpdateUserMiddleware,
-	adminCheckMiddleware,
+
 	read
+	//we do not need to apply middleware here , bcz it has to be publicly visible
 );
 
 router.put(
