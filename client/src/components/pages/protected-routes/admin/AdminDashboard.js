a563bfa450c228e17ce6b83868fb9d5e37a266a6
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-05-19 20:42:45 +0530

    Update Admin-dashboard.js

--- client/src/components/pages/protected-routes/admin/AdminDashboard.js
+++ client/src/components/pages/protected-routes/admin/AdminDashboard.js
@@ -1,9 +1,15 @@
 import React from "react";
+import AdminNav from "../../../Nav/Admin-Nav";
 
 export const AdminDashboard = () => {
 	return (
-		<div>
-			<h2>Welcome, Admin Dashboard</h2>
+		<div className="container-fluid">
+			<div className="row">
+				<div className="col-md-2">
+					<AdminNav />
+				</div>
+				<div className="col">Welcome Admin to your dashboard.</div>
+			</div>
 		</div>
 	);
 };
