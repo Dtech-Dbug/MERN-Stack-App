7d213651d0884f9ce3856fbe1beb0bdec163d92a
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-05-20 13:12:50 +0530

    updated categoryCreate.js

--- client/src/components/pages/protected-routes/admin/Category/categoryCreate.js
+++ client/src/components/pages/protected-routes/admin/Category/categoryCreate.js
@@ -1,14 +1,80 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AdminNav from "../../../../Nav/Admin-Nav";
+import { toast } from "react-toastify";
+import { EditTwoToned, DeleteTwoToned } from "@ant-design/icons";
+//access state : for user authtoken
+import { useSelector } from "react-redux";
+//function for interacting w backend
+import {
+	getCategoryLists,
+	createCategory,
+	removeCategory,
+} from "../../../../../functions/categoryCRUD";
 
 export const CreateCategory = () => {
+	const { user } = useSelector((state) => ({ ...state }));
+	const [name, setName] = useState("");
+	const [categoriesList, setCategoriesLists] = useState([]);
+
+	//to render the lists of category, using useEffect
+
+	useEffect(() => {
+		loadLists();
+	}, []);
+
+	const loadLists = await getCategoryLists()
+		.then((res) => {
+			console.log(res);
+		})
+		.catch((err) => {
+			toast.error(`A booboo happened`);
+		});
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		console.log(name);
+		createCategory({ name }, user.token)
+			.then((res) => {
+				setCategoriesLists(res.data.name);
+				toast.success(`${name} has been created`);
+			})
+			.catch((err) => {
+				console.log(err);
+				toast.error(`Something went wrong!`);
+			});
+	}
+	const categoryForm = () => {
+		return (
+			<form onSubmit={handleSubmit}>
+				<div className="form-group">
+					<label>
+						<h3>Name:</h3>
+					</label>
+					<input
+						placeholder="Create Category"
+						className="form-control"
+						type="text"
+						autoFocus
+						required
+						onChange={(e) => setName(e.target.value)}
+					/>
+					<button className="btn btn-raised btn-primary mt-2">Save</button>
+				</div>
+			</form>
+		);
+	};
 	return (
 		<div className="container-fluid">
 			<div className="row">
 				<div className="col-md-2">
 					<AdminNav />
 				</div>
-				<div className="col">Welcome Admin. Ready to create categories?</div>
+				<div className="col">
+					<h2 className="text-warning">
+						Welcome Admin. Ready to create categories?
+					</h2>
+					{categoryForm()}
+				</div>
 			</div>
 		</div>
 	);
