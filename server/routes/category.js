e2e57ed465750f6c6b80937668f86183bbcdc56a
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-05-27 00:46:10 +0530

    updated category.js : commented out not used controllers

--- server/routes/category.js
+++ server/routes/category.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 //controllers
-const { createOrUpdateUser, currentUser } = require("../controllers/auth");
+//const { createOrUpdateUser, currentUser } = require("../controllers/auth");
 
 //middlewares
 const {
