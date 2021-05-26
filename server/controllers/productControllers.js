f64e191dff44a2d3a20271c764133e657f04e00e
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-05-27 00:45:44 +0530

    created productControllers.js

---
+++ server/controllers/productControllers.js
@@ -0,0 +1,15 @@
+const ProductModel = require("../model/productModel");
+const slugify = require("slugify");
+
+exports.createProduct = async (req, res) => {
+	//
+	try {
+		console.log("Request Body ------> ", req.body);
+		req.body.slug = slugify(req.body.title);
+		const New_Product = await new ProductModel(req.body).save();
+		res.json(New_Product);
+	} catch (err) {
+		console.log(err);
+		res.status(400).send("Category Creation Failed , due to ");
+	}
+};
