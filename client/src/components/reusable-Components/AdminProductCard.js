af6e00f53174e04cea7f07cdf8ac5b409fbf7c93
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-01 18:56:18 +0530

    docs 🚀: created AdminProductCard.js : reusbale component

---
+++ client/src/components/reusable-Components/AdminProductCard.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Card } from "antd";
+
+const { Meta } = Card;
+
+const AdminProductCard = ({ product }) => {
+	// destructure
+	const { title, description, images } = product;
+
+	return (
+		<Card
+			cover={
+				<img
+					src={images && images.length ? images[0].url : ""}
+					style={{ height: "150px", objectFit: "cover" }}
+					className="p-1"
+				/>
+			}
+		>
+			<Meta title={title} description={description} />
+		</Card>
+	);
+};
+
+export default AdminProductCard;
