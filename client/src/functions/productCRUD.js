c0cd8204ed51147becd3a576432d4270255ea5a0
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-01 20:34:13 +0530

    update 🚀: productCRUD : removeProduct function created

--- client/src/functions/productCRUD.js
+++ client/src/functions/productCRUD.js
@@ -11,3 +11,11 @@ export const createProduct = async (product, authtoken) => {
 export const listAllProducts = async (count) => {
 	return axios.get(`http://localhost:8000/api/products/${count}`);
 };
+
+export const removeProduct = async (slug, authtoken) => {
+	return await axios.delete(`http://localhost:8000/product/${slug}`, {
+		headers: {
+			authtoken,
+		},
+	});
+};
