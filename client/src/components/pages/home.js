959316a7de4196ed3125c704e85d0e69ab4478ef
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-17 21:22:11 +0530

    update 🚀: Home.js component
    - Setting up the component
    - fetching all products and JSON Stringified the response

--- client/src/components/pages/home.js
+++ client/src/components/pages/home.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { listAllProducts } from "../../functions/productCRUD";
 
-export const Home = ()=>{
+export const Home = () => {
+	const [products, setProducts] = useState([]);
+	useEffect(() => {
+		loadAllProducts();
+	}, []);
 
-return (
-    <h2>
-        Home
-    </h2>
-)
+	const loadAllProducts = () => {
+		listAllProducts(5).then((res) => {
+			setProducts(res.data);
+		});
+	};
 
-}
+	return (
+		<>
+			<h2>Home</h2>;{JSON.stringify(products)}
+		</>
+	);
+};
