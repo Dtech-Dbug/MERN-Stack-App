4bec8d0ed0455526d8ee209e33a896d93263c4e0
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-18 13:17:13 +0530

    docs () 🚀: create LoadingCardComponent.js
    - Loading card component created
    - Imported Skeleteon fron antd

---
+++ client/src/components/reusable-Components/LoadingCardComponent.js
@@ -0,0 +1,8 @@
+import { Skeleton } from "antd";
+import React from "react";
+
+const LoadingCardComponent = () => {
+	return <Skeleton active className="col-md-4"></Skeleton>;
+};
+
+export default LoadingCardComponent;
