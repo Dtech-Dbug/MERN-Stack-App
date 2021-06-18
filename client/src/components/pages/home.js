91b1878563b3f74c952ba1c701a75a1d7b473466
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-18 13:18:36 +0530

    update 🚀: home.js => component
    - Imported LoadingCardComponent.
    - testing -> working well

--- client/src/components/pages/home.js
+++ client/src/components/pages/home.js
@@ -1,17 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { listAllProducts } from "../../functions/productCRUD";
 import HomePageProductCard from "../reusable-Components/HomePageProductCard";
+import LoadingCardComponent from "../reusable-Components/LoadingCardComponent";
 import TypewriterComponent from "../reusable-Components/TypewriterComponent";
 
 export const Home = () => {
 	const [products, setProducts] = useState([]);
+	const [loading, setLoading] = useState(false);
 	useEffect(() => {
 		loadAllProducts();
 	}, []);
 
 	const loadAllProducts = () => {
+		setLoading(true);
 		listAllProducts(3).then((res) => {
 			setProducts(res.data);
+			setLoading(true);
 		});
 	};
 
@@ -24,15 +28,19 @@ export const Home = () => {
 				/>
 			</div>
 			<hr />
-			<div className="container">
-				<div className="row">
-					{products.map((product) => (
-						<div key={product._id} className="col-md-4">
-							<HomePageProductCard product={product} />
-						</div>
-					))}
+			{loading ? (
+				<LoadingCardComponent />
+			) : (
+				<div className="container">
+					<div className="row">
+						{products.map((product) => (
+							<div key={product._id} className="col-md-4">
+								<HomePageProductCard product={product} />
+							</div>
+						))}
+					</div>
 				</div>
-			</div>
+			)}
 		</>
 	);
 };
