f5879b07e117598ef722be6ae414e64b85c35eaf
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-21 14:22:23 +0530

    create docs 🚀: BestSellers.js
    - seperate component for the best sellers component

---
+++ client/src/components/pages/HomePageSections/BestSellers.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import {
+	listAllProducts,
+	listOrderedProducts,
+} from "../../../functions/productCRUD";
+import HomePageProductCard from "../../reusable-Components/HomePageProductCard";
+import LoadingCardComponent from "../../reusable-Components/LoadingCardComponent";
+
+export const BestSellers = () => {
+	const [products, setProducts] = useState([]);
+	const [loading, setLoading] = useState(false);
+	useEffect(() => {
+		loadAllProducts();
+	}, []);
+
+	const loadAllProducts = () => {
+		setLoading(true);
+		listOrderedProducts("sold", "desc", 3).then((res) => {
+			setProducts(res.data);
+			console.log("res from new funtion ", res);
+			setLoading(false);
+		});
+	};
+
+	return (
+		<>
+			<div className="container">
+				{loading ? (
+					<LoadingCardComponent count={3} />
+				) : (
+					<div className="row">
+						{products.map((product) => (
+							<div key={product._id} className="col-md-4">
+								<HomePageProductCard product={product} />
+							</div>
+						))}
+					</div>
+				)}
+			</div>
+		</>
+	);
+};
