590d37d1f7a6396e726a25027f01251a71474bb4
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-22 13:56:33 +0530

    update , fix 🩺client/pages/viewProduct.js
    - Fixed a silly error
    - objects can not be rendered as react child
    - JSON.stringify(prodcut array) to vie w and handle the response

--- client/src/components/pages/ViewProduct.js
+++ client/src/components/pages/ViewProduct.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { readProduct } from "../../functions/productCRUD";
 
-const ViewProduct = () => {
+const ViewProduct = ({ match }) => {
+	const { slug } = match.params;
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		readProduct(slug).then((res) => {
+			console.log("Response in the single product", res.data);
+			setProducts(res.data);
+		});
+	}, []);
+
 	return (
 		<div>
 			<h1>Welcome</h1>
+			<h3>{slug}</h3>
+
+			<br />
+			<br />
+
+			{JSON.stringify(products)}
 		</div>
 	);
 };
