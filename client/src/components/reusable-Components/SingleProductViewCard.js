ff911c318c11ee09ca423d32979adbfbc3d48cb8
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-22 21:38:31 +0530

    Update 🚀: SingleProductView.js
    - imported Carousel component from react-responsive-carousel
    
    - used the component to display product  images

--- client/src/components/reusable-Components/SingleProductViewCard.js
+++ client/src/components/reusable-Components/SingleProductViewCard.js
@@ -3,6 +3,11 @@ import { Link } from "react-router-dom";
 import { Card } from "antd";
 import { ShoppingCartOutlined, HeartOutlined } from "@ant-design/icons";
 
+// import caraousel from react-responsive-caraousel
+import {Carousel} from 'react-responsive-carousel'
+//import css files for react-responsive-Carousel
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
+
 const { Meta } = Card;
 const SingleProductViewCard = ({ products }) => {
 	const { title, description, slug, images } = products;
@@ -10,6 +15,11 @@ const SingleProductViewCard = ({ products }) => {
 		<>
 			<div className="col-md-7">
 				<h2>Image craousel</h2>
+				<Carousel showArrows={true} autoPlay infiniteLoop>
+					{images && images.map((i) => {
+						 return <img src={i.url} key={i.public_id} alt='oops' />
+					})}
+				</Carousel>
 			</div>
 
 			<div className="col-md-5">
