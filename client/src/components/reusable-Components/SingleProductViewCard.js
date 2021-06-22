8ab69220e59f325b018af5993c3af9e9323b9521
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-22 14:08:29 +0530

    create ✍: SingleProductViewCard.js
    - created dummy component for test
    - working

---
+++ client/src/components/reusable-Components/SingleProductViewCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SingleProductViewCard = () => {
+	return (
+		<div>
+			<h1>Welcome toview the single product</h1>
+		</div>
+	);
+};
+
+export default SingleProductViewCard;
