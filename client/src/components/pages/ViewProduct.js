b059d25a3de35b7d0ccb16bbc4adea2193b00aff
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-22 12:10:33 +0530

    Update 🚀: renamed SingleProduct to viewProduct.js

--- client/src/components/pages/ViewProduct.js
+++ client/src/components/pages/ViewProduct.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const SingleProduct = () => {
+const ViewProduct = () => {
 	return (
 		<div>
 			<h1>Welcome</h1>
@@ -8,4 +8,4 @@ const SingleProduct = () => {
 	);
 };
 
-export default SingleProduct;
+export default ViewProduct;
