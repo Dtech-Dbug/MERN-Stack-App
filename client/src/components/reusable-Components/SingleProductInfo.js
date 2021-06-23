53f585acc4f97c6b6bf900398d80c0c48ffcccfc
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-23 13:12:28 +0530

    update 🚀: SingleProductInfo.js
    - destructured all the product components
    
    - listed them

--- client/src/components/reusable-Components/SingleProductInfo.js
+++ client/src/components/reusable-Components/SingleProductInfo.js
@@ -1,11 +1,71 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const SingleProductInfo = () => {
-    return (
-        <div>
-            Hello
-        </div>
-    )
-}
+const SingleProductInfo = ({ product }) => {
+	const { price, category, subCategories, shipping, color, quantity, sold } =
+		product;
+	return (
+		<ul className="list-group">
+			<li className="list-group-item">
+				Price{" "}
+				<span className="label label-default label-pill pull-xs-right ">
+					$ {price}
+				</span>
+			</li>
+			{category && (
+				<li className="list-group-item">
+					Category{" "}
+					<Link
+						to={`/category/${category.slug}`}
+						className="label label-default label-pill pull-xs-right"
+					>
+						{category.name}
+					</Link>
+				</li>
+			)}
+			<li className="list-group-item">
+				Sub Categories{" "}
+				{subCategories &&
+					subCategories.map((s) => {
+						return (
+							<Link
+								to={`subcategory/${s.slug}`}
+								key={s._id}
+								className="label label-default label-pill pull-xs-right "
+							>
+								{s.name}
+							</Link>
+						);
+					})}
+			</li>
+			<li className="list-group-item">
+				Shipping{" "}
+				<span className="label label-default label-pill pull-xs-right ">
+					{shipping}
+				</span>
+			</li>
+			<li className="list-group-item">
+				Color{" "}
+				<span className="label label-default label-pill pull-xs-right ">
+					{color}
+				</span>
+			</li>
+			<li className="list-group-item">
+				In Stock
+				<span className="label label-default label-pill pull-xs-right ">
+					{quantity}
+				</span>
+			</li>
+			<li className="list-group-item">
+				Sold Items
+				<span className="label label-default label-pill pull-xs-right ">
+					{sold}
+				</span>
+			</li>
+		</ul>
+	);
+};
 
-export default SingleProductInfo
+export default SingleProductInfo;
+
+//pull-xs-right => Is the className that moves the content to the right in the card
