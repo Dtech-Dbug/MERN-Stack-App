9ca752e1ac069eb16f02d43853ec26519e336066
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-24 08:10:18 +0530

    update 🚀: productController.js : rateProduct function
    - updated controllers for rating system

--- server/controllers/productControllers.js
+++ server/controllers/productControllers.js
@@ -138,4 +138,65 @@ exports.getProductsCount = async (req, res) => {
 	res.json(TotalProductsCount);
 };
 
-exports.rateProduct = async (req, res) => {};
+exports.rateProduct = async (req, res) => {
+	//1. Find the product based on the productId
+	//we can get the productId from req.params.productId
+	const product = await ProductModel.findById(req.params.productId).exec();
+
+	//2. we also need to find the user, for rating
+	const user = await User.findOne({ email: req.user.email }).exec();
+
+	//3. Who is rating or updating the rating.[we need to update rating, in case where user has already rated. Instead of repeating]
+	//check if user logged-in user has already rated
+
+	//4. destructur star from req.body => bcz we will send star from frontend
+	const { star } = req.body;
+	//whole idea =>
+	//we find the product
+	//add the star by the loggedIn user
+
+	//refer to the ProductModel. We have ratings array => which is an array of objects
+	//to check if user has already rated = we find for the userId in the rating object. Then update the rating.
+	//If user has not rated previosly , we push the new rating
+	//We have stored the product after quering our databse by the productId in the 'product' variable
+	//we can easily access the ratings object of a single product(based on productId) from the productModel
+
+	//5. we use the find method on the rating array to check , if the postedBy Id equals to the currently loggedIn user's ID
+	//If it is true => then we get matching element object. [meaning user already rated]
+	//or we get undefined [meaning: user has not rated the product yet]
+	let existingRatingObject = product.ratings.find(
+		(element) => element.postedBy == user._id
+	);
+
+	//based on the existingratingProduct variable , we can add or update ratings.
+
+	//If user has not rated before => then we push the new one in the rating array
+	if (existingRatingObject === undefined) {
+		//existingRatingObject === undefined means => user have not rated the product before
+		//query the ProductModelbased on productId , use  findOneandUpdate Mongoose Method
+		//takes two args => query , and what to update
+		let addRating = await ProductModel.findByIdAndUpdate(
+			product._id,
+			{
+				//Now , what to update?
+				//we push the new rating to the ratings array using a special method => $push
+				$push: { ratings: { star: star, postedBy: user._id } }, //these are the object that comprises the ratings array
+			},
+			{ new: true }
+		).exec();
+
+		console.log("Rating added:::", addRating);
+		res.json(addRating);
+	}
+	//else we updatethe existing array if user has already rated the product once.
+	else {
+		const updateRating = await ProductModel.updateOne(
+			//we find the product in the ratings array. But which one? The one stored in existingRating Object
+			{ ratings: { $elemMatch: existingRatingObject } }, //query? matching elemet if it matches the existingRating object
+			{ $set: { "ratings.$.star": star } }, //what to update? the star [number] only. using a special property $set
+			{ new: true }
+		).exec();
+		res.json(updateRating);
+		console.log("Rating Updated:::", updateRating);
+	}
+};
