359a0f3db9281355736a73e914e0bb91c5bac4c3
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-25 19:52:44 +0530

    Update 🚀: viewProduct.js
    - Imported the FrontEnd function to send the ratings data to backend.
    
    - Sent the ratings data to backend
    
    - and checked the response, working fine.

--- client/src/components/pages/ViewProduct.js
+++ client/src/components/pages/ViewProduct.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { readProduct } from "../../functions/productCRUD";
+import { readProduct, productRating } from "../../functions/productCRUD";
 import SingleProductViewCard from "../reusable-Components/SingleProductViewCard";
+import { useSelector } from "react-redux";
 
 const ViewProduct = ({ match }) => {
 	const { slug } = match.params;
@@ -9,12 +10,18 @@ const ViewProduct = ({ match }) => {
 	//state of stars, instead of hard coding
 	const [star, setStar] = useState(0);
 
+	//access to the usrr, to pass the user.token
+	const { user } = useSelector((state) => ({ ...state }));
+
 	useEffect(() => {
+		loadProduct();
+	}, []);
+	const loadProduct = () => {
 		readProduct(slug).then((res) => {
 			console.log("Response in the single product", res.data);
 			setProduct(res.data);
 		});
-	}, []);
+	};
 
 	function onStarClick(newRating, name) {
 		//name => id of the product which is being rated
@@ -22,6 +29,10 @@ const ViewProduct = ({ match }) => {
 
 		//setStar ti=o newrating=> whatver star number we useri is selecting
 		setStar(newRating);
+		productRating(name, newRating, user.token).then((res) => {
+			console.log("Rating Given", res);
+			loadProduct(); //immediately reflecet change
+		});
 	}
 
 	return (
