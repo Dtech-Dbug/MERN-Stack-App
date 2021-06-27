761e65ed80bf4abc1790bc57317b072deb1ea82f
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-27 13:54:37 +0530

    update : show ratings in home page
    - implemented the showAverageRating function in HomePageProductCard.js component

--- client/src/components/reusable-Components/HomePageProductCard.js
+++ client/src/components/reusable-Components/HomePageProductCard.js
@@ -3,36 +3,46 @@ import { Card } from "antd";
 import { Link } from "react-router-dom";
 import JS from "../../Default images/js logo.png";
 import { ShoppingCartOutlined, EyeOutlined } from "@ant-design/icons";
+import { showAverageRating } from "../../functions/rating";
 const { Meta } = Card;
 
 const HomePageProductCard = ({ product }) => {
 	const { images, title, description, slug } = product;
 	return (
-		<Card
-			className="m-3"
-			style={{ border: "1px solid black" }}
-			cover={
-				<img
-					src={images && images.length ? images[0].url : JS}
-					style={{ height: "150px", objectFit: "cover" }}
-					className="col-md-6 offset-md-2 mt-1"
-					alt="oops"
-				/>
-			}
-			actions={[
-				<>
-					<Link to={`/products/${slug}`}>
-						<EyeOutlined className="text-warning" /> <br /> view Prouct
-					</Link>
-				</>,
+		<>
+			<div className="text-center">
+				{product && product.ratings && product.ratings.length > 0 ? (
+					showAverageRating(product)
+				) : (
+					<div className="text-center pt-1 pb-3">No rating yet</div>
+				)}
+			</div>
+			<Card
+				className="m-3"
+				style={{ border: "1px solid black" }}
+				cover={
+					<img
+						src={images && images.length ? images[0].url : JS}
+						style={{ height: "150px", objectFit: "cover" }}
+						className="col-md-6 offset-md-2 mt-1"
+						alt="oops"
+					/>
+				}
+				actions={[
+					<>
+						<Link to={`/products/${slug}`}>
+							<EyeOutlined className="text-warning" /> <br /> view Prouct
+						</Link>
+					</>,
 
-				<>
-					<ShoppingCartOutlined className="text-warning" /> <br /> Add To Cart
-				</>,
-			]}
-		>
-			<Meta title={title} description={description} />
-		</Card>
+					<>
+						<ShoppingCartOutlined className="text-warning" /> <br /> Add To Cart
+					</>,
+				]}
+			>
+				<Meta title={title} description={description} />
+			</Card>
+		</>
 	);
 };
 
