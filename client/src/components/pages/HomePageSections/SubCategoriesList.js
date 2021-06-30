62a4fd0c7ce55f2746ffe1b8518d5d74f5b4ad38
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-30 11:53:38 +0530

    create 🚀: HomePageSections/subCategoriesList.js
    - created a component for displaying all the subCategoruesList.js

---
+++ client/src/components/pages/HomePageSections/SubCategoriesList.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import { getSubcategoryLists } from "../../../functions/subCategoryCrud";
+import { Link } from "react-router-dom";
+
+const SubCategoriesList = () => {
+	const [loading, setLoading] = useState(false);
+	const [subCategories, setSubCategories] = useState([]);
+
+	useEffect(() => {
+		getSubcategoryLists().then((res) => {
+			setLoading(true);
+			setSubCategories(res.data);
+			setLoading(false);
+		});
+	}, []);
+
+	const showCategories = () =>
+		subCategories &&
+		subCategories.map((c) => {
+			return (
+				<div
+					key={c._id}
+					className="col btn btn-outlined-primary btn-lg btn-raised m-3"
+				>
+					<Link to={`/category/${c.slug}`}>{c.name}</Link>
+				</div>
+			);
+		});
+
+	return (
+		<div className="container">
+			<div className="row">
+				{loading ? (
+					<h4 className="text-center">Loading...</h4>
+				) : (
+					showCategories()
+				)}
+			</div>
+		</div>
+	);
+};
+
+export default SubCategoriesList;
