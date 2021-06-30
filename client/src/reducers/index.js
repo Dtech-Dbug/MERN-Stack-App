0cf1f8dbcd3a1e55a3ab1fb025645427d8bfde03
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-06-30 21:37:41 +0530

    reducers.js : imported SearchReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import { userReducer } from "./userReducer";
+import { searchReducer } from "./search";
 
 export const rootReducer = combineReducers({
 	user: userReducer,
+	search: searchReducer,
 });
