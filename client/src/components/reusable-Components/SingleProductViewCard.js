68dd568c99f6aabe59948bda5478e4075e472f34
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-04 22:07:46 +0530

    Update 🚀: SingleProductViewViewCard.js
    - applied same handleAddToCart function in this component
    
    - saved product in localStorage and then save the product in globally in redux store

--- client/src/components/reusable-Components/SingleProductViewCard.js
+++ client/src/components/reusable-Components/SingleProductViewCard.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import SingleProductInfo from "./SingleProductInfo";
 import { Link } from "react-router-dom";
-import { Card, Tabs } from "antd";
+import { Card, Tabs, Tooltip } from "antd";
 import { ShoppingCartOutlined, HeartOutlined } from "@ant-design/icons";
 import StarRatings from "react-star-ratings";
 import { showAverageRating } from "../../functions/rating";
+import { useSelector, useDispatch } from "react-redux";
+import _ from "lodash";
 
 //import modal component
 import RatingModal from "../Modal/RatingModal";
@@ -17,7 +19,45 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 const { Meta } = Card;
 const { TabPane } = Tabs;
 const SingleProductViewCard = ({ product, onStarClick, star }) => {
+	const [tooltip, setTooltip] = useState("Click to add to cart");
 	const { title, images, description, _id, ratings } = product;
+	const { user, cart } = useSelector((state) => ({ ...state }));
+	const dispatch = useDispatch();
+
+	const handleAddToCart = () => {
+		//create an array of cart to save the array in LocalStorgae
+		let cart = [];
+
+		//check if window === true , add cart to localStorage
+		if (typeof window !== undefined) {
+			//and localStorage has the cart already = case when user has already added one to cart
+			if (localStorage.getItem("cart")) {
+				//use JSON.parse to get the stored data a JS objects
+				cart = JSON.parse(localStorage.getItem("cart"));
+			}
+			//push newProduct to cart
+			//spread the product prop : to access all it's values
+			//and add a new key called count to the product object
+			cart.push({
+				...product,
+				count: 1,
+			});
+
+			//remove duplicate
+			//using lodash for that = uniqwith method of lodash
+			let unique = _.uniqWith(cart, _.isEqual);
+
+			//save the new items to LS, when user adds to cart for forst time
+			localStorage.setItem("cart", JSON.stringify(unique));
+			setTooltip("Added");
+
+			//add to redux
+			dispatch({
+				type: "ADD_TO_CART",
+				payload: cart,
+			});
+		}
+	};
 	return (
 		<>
 			<div className="col-md-7">
@@ -48,10 +88,12 @@ const SingleProductViewCard = ({ product, onStarClick, star }) => {
 
 				<Card
 					actions={[
-						<>
-							<ShoppingCartOutlined className="text-success" /> <br />
-							Add to Cart
-						</>,
+						<Tooltip title={tooltip}>
+							<a onClick={handleAddToCart}>
+								<ShoppingCartOutlined className="text-success" /> <br />
+								Add to Cart
+							</a>
+						</Tooltip>,
 						<Link to="/">
 							<HeartOutlined className="text-info" /> <br /> Add to Wishlist
 						</Link>,
