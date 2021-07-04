ee86ac04e8a2f0f9e499ea89de41a0737ac2521b
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-04 21:42:42 +0530

    added reducer for add-to-cart
    created reducer : cartreducer

---
+++ client/src/reducers/cartReducer.js
@@ -0,0 +1,20 @@
+let initialState = [];
+
+//load from LocalStorage
+if (typeof window !== "undefined") {
+	if (localStorage.getItem("cart")) {
+		initialState = JSON.parse(localStorage.getItem("cart"));
+	} else {
+		initialState = [];
+	}
+}
+
+export const userReducer = (state = initialState, action) => {
+	switch (action.type) {
+		case "ADD_TO_CART":
+			return action.payload;
+
+		default:
+			return state;
+	}
+};
