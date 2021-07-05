60eed56eff8e66845fae6882cd93648c9ec362e4
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-05 23:24:47 +0530

    Update 🚀: ProductCartTableView
    - imported ModalImage
    
    - showed the product images woth ModalImage

--- client/src/components/reusable-Components/ProductCartTableView.js
+++ client/src/components/reusable-Components/ProductCartTableView.js
@@ -1,10 +1,23 @@
 import React from "react";
+import ModalImage from "react-modal-image";
+import JS from "../../Default images/js logo.png";
 
 const ProductCartTableView = ({ product }) => {
 	return (
 		<tbody>
 			<tr>
-				<td>Image</td>
+				<td>
+					<div style={{ width: "100px", height: "auto" }}>
+						{product.images ? (
+							<ModalImage
+								small={product.images[0].url}
+								large={product.images[0].url}
+							/>
+						) : (
+							<ModalImage small={JS} large={JS} />
+						)}
+					</div>
+				</td>
 				<td>{product.title}</td>
 				<td>${product.price}</td>
 				<td>Color </td>
