353ae40eeeb1fefc293ee8393d3e34c2113a44b1
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-06 21:22:44 +0530

    Update : DrawerCrad
    - set visible to drawer : state in redux

--- client/src/components/Drawer/DrawerCard.js
+++ client/src/components/Drawer/DrawerCard.js
@@ -5,7 +5,7 @@ import { Drawer, Button } from "antd";
 const DrawerCard = ({ children }) => {
 	const { drawer, cart } = useSelector((state) => ({ ...state }));
 
-	return <Drawer visible={true}>{JSON.stringify(cart)}</Drawer>;
+	return <Drawer visible={drawer}>{JSON.stringify(cart)}</Drawer>;
 };
 
 export default DrawerCard;
