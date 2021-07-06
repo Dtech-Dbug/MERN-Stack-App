fe32bff5bd41791e220858ecf85bba1f77c1effe
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-06 19:40:16 +0530

    update 🚀: reducers/index.js
    - imported recently created drawerReducer
    
    - combined the reducers and exported rootReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import { userReducer } from "./userReducer";
 import { searchReducer } from "./search";
 import { cartReducer } from "./cartReducer";
+import { drawerReducer } from "./drawerReducer";
 
 export const rootReducer = combineReducers({
 	user: userReducer,
 	search: searchReducer,
 	cart: cartReducer,
+	drawer: drawerReducer,
 });
