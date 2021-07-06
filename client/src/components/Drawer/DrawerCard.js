c72efe48f6de8ce49ba300a2aae340ff167c2628
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-06 21:31:07 +0530

    update 🚀: DrawerCard.js
    - fixed : close drawer component when clicking on anywhere on the screen

--- client/src/components/Drawer/DrawerCard.js
+++ client/src/components/Drawer/DrawerCard.js
@@ -1,11 +1,24 @@
 import React from "react";
-import { useSelector, useDispacth } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { Drawer, Button } from "antd";
 
 const DrawerCard = ({ children }) => {
 	const { drawer, cart } = useSelector((state) => ({ ...state }));
+	const dispatch = useDispatch();
 
-	return <Drawer visible={drawer}>{JSON.stringify(cart)}</Drawer>;
+	return (
+		<Drawer
+			onClose={() =>
+				dispatch({
+					type: "SHOW_CART_DRAWER",
+					payload: false,
+				})
+			}
+			visible={drawer}
+		>
+			{JSON.stringify(cart)}
+		</Drawer>
+	);
 };
 
 export default DrawerCard;
