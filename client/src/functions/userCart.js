f36baaaa565ed146e0f2fbf57cd429a4d6883f7e
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-07 22:23:49 +0530

    update 🚀 : client/functions/usercart.js
    - created another frontEnd function to fetch data from backend , user and cart

--- client/src/functions/userCart.js
+++ client/src/functions/userCart.js
@@ -13,3 +13,10 @@ export const userCart = async (cart, authtoken) =>
 
 //wrapping the cart in {} to be able to access it in the backend from req.body.cart
 //if we didn't wrap we would get req,body to the backened , and then destructure {cart} from req.body
+
+export const getUserCart = async (authtoken) =>
+	await axios.get(`http://localhost:8000/api/user/cart`, {
+		headers: {
+			authtoken,
+		},
+	});
