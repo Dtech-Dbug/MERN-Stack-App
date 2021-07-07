485c759267ab960a33202f0e120b6e2c4bd2ae6f
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-07 12:11:33 +0530

    docs /model : model/Cart.js
    - created and exported schema for cart model to save cart Items in the DB based on user and also refer to productModel

---
+++ server/model/cart.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const { ObjectId } = mongoose.Schema;
+
+const cartSchema = new mongoose.Schema(
+	{
+		products: [
+			{
+				product: {
+					type: ObjectId,
+					ref: "ProductModel",
+				},
+				count: Number,
+				color: String,
+				price: Number,
+			},
+		],
+
+		cartTotal: Number,
+		totalAfterDiscount: Number,
+		orderedBy: { type: ObjectId, ref: "User" },
+	},
+	{ timestamps: true }
+);
+
+module.exports = mongoose.model("CartModel", cartSchema);
