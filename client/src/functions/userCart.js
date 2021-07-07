9eeb8880c98f814419316dad06c91d89a6366e18
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-07 11:47:13 +0530

    Create : functions.userCart : a function to save cartItems to the backend : user Database

---
+++ client/src/functions/userCart.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export const userCart = (cart, authtoken) =>
+	axios.post(
+		`http://localhost:8000/api/cart`,
+		{ cart },
+		{ headers: { authtoken } }
+	);
+
+//wrapping the cart in {} to be able to access it in the backend from req.body.cart
+//if we didn't wrap we would get req,body to the backened , and then destructure {cart} from req.body
