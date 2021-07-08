6ef910dd4ed4d45acf6dac68860b51e7eab9a1e9
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-08 17:07:27 +0530

    update 🚀: server/routes/user.js
    - created a route to save user address
    
    - used and imported a controller - yet to define the controller

--- server/routes/user.js
+++ server/routes/user.js
@@ -12,10 +12,12 @@ const {
 	userCart,
 	getUserCart,
 	emptyUserCart,
+	userAddress,
 } = require("../controllers/userCart");
 
 router.post("/user/cart", createOrUpdateUserMiddleware, userCart); //save cartItems in DB
 router.get("/user/cart", createOrUpdateUserMiddleware, getUserCart); //get user cart info from out own backend
-router.delete("/user/cart", createOrUpdateUserMiddleware, emptyUserCart);
+router.delete("/user/cart", createOrUpdateUserMiddleware, emptyUserCart); // empty cart
+router.post("/user/address", createOrUpdateUserMiddleware, userAddress); //save address of user
 
 module.exports = router;
