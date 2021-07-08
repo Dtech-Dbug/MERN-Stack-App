30a29ca766b6e53b84569301f245caf6c06f475f
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-08 11:23:21 +0530

    Update & fix 🚀: pages/checkOut.js
    - created state for products and cartTotall
    
    error : can not read property token of null, in the useEffect trying to access user.token from redux state.
    
    cause => when component mounts for the first time, the user state is null , after some time it changes to the loggedIn user's state.
    Due to the null state when component mounts , the error of can not read property token of null was shown.
    user. token , meant [null.token == null] until the state is populated with the loggedIn user's state.
    
    fix : check if user exists then run the function that requires user.token , and set the deps array to user => meaning the function runs only when user changes
    
    on mount user changes from null -> loggedIn user

--- client/src/components/pages/Checkout.js
+++ client/src/components/pages/Checkout.js
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { getUserCart } from "../../functions/userCart";
 
 const Checkout = () => {
+	//state of products
+	const [cartProducts, setCartProducts] = useState([]);
+	const [cartTotal, setCartTotal] = useState(0);
+
+	const { user } = useSelector((state) => ({ ...state }));
+
+	useEffect(() => {
+		// let token = user.token;
+		// console.log("user token ", token);
+		// getUserCart(token).then((res) =>
+		// 	console.log("response from DB of user Cart in checkout", res)
+		// );
+		user && fetchCart();
+	}, [user]);
+
+	const fetchCart = () =>
+		getUserCart(user.token).then((res) => console.log("res from cart BE", res));
+
 	function saveAddressToDb() {
 		//
 	}
