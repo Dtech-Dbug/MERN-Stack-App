0202aef3a675c2f9ec508009a8bdfb579d4e6b94
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-10 19:21:16 +0530

    create Controllers : server/controllers/couponControllers
    - defined the controllers
    
    - create , list and remove controller functions of coupons

---
+++ server/controllers/couponControllers.js
@@ -0,0 +1,30 @@
+const CouponModel = require("../model/couponModel");
+
+// create
+exports.create = async (req, res) => {
+	try {
+		//to create we need information from fronTend
+		//we destructure them from req.body
+		console.log("Coupon body ===>", req.body);
+		const { name, expiry, discount } = req.body;
+
+		const saveCoupon = await new CouponModel({ name, expiry, discount }).save();
+		res.json(saveCoupon);
+	} catch (err) {
+		console.log("error while saving coupons", err);
+	}
+};
+
+//list
+exports.list = async (req, res) => {
+	res.json(await CouponModel.find({}).sort({ createdAt: -1 }).exec());
+};
+
+//remove
+exports.remove = async (req, res) => {
+	try {
+		res.json(await CouponModel.findByIdAndDelete(req.params.couponId).exec());
+	} catch (err) {
+		console.log("err while deleting coupon", err);
+	}
+};
