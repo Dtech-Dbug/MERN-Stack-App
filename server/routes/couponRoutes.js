908ab63988c38bbd55c57773dba1651fa511e4e9
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-10 18:59:23 +0530

    create routes : server/routes/couponController.js
    - imported and used middlewares
    
    - created endPoints
    
    - imported and used controller functions : yet to define them

---
+++ server/routes/couponRoutes.js
@@ -0,0 +1,30 @@
+const express = require("exptess");
+
+const router = express();
+
+//middlewares
+const {
+	createOrUpdateUserMiddleware,
+	adminCheckMiddleware,
+} = require("../middlewares/authMiddleware");
+
+//controllers
+
+const { create, remove, list } = require("../controllers/couponControllers");
+
+//routes
+router.post(
+	"/coupon",
+	createOrUpdateUserMiddleware,
+	adminCheckMiddleware,
+	create
+);
+router.get("/coupons", list);
+router.delete(
+	"/coupon/:couponId",
+	createOrUpdateUserMiddleware,
+	adminCheckMiddleware,
+	remove
+);
+
+module.exports = router;
