d459a23e9c9007040ca754b2480aa98c6cc42be5
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-10 18:39:29 +0530

    create Schema : server/Models/CouponModel
    -created the schema for coupon

---
+++ server/model/couponModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const couponSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			trim: true,
+			unique: true,
+			uppercase: true,
+			required: "Name is Required",
+			minlength: [6, "Invalid.Too Short"],
+			maxlength: [12, "Invalid.Too Long"],
+		},
+		expiry: {
+			type: Date,
+			required: true,
+		},
+
+		discount: {
+			type: Number,
+			required: true,
+		},
+	},
+	{ timestamps: true }
+);
+
+module.exports = mongoose.model("CouponModel", couponSchema);
