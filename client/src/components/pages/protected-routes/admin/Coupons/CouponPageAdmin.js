74d3ec5157420f801cdfd2391ec0c9a373c8b2b2
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-10 21:09:27 +0530

    docs : updated : couponPageAdmin.js

--- client/src/components/pages/protected-routes/admin/Coupons/CouponPageAdmin.js
+++ client/src/components/pages/protected-routes/admin/Coupons/CouponPageAdmin.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import {} from "../../../../../functions/coupons";
+import AdminNav from "../../../../Nav/Admin-Nav";
 
 const CouponPageAdmin = () => {
 	return (
-		<div className="container">
-			<h1>Coupon</h1>
+		<div className="container-fluid">
+			<div className="row">
+				<div className="col-md-2">
+					<AdminNav />
+				</div>
+
+				<div className="col-md-10">
+					<h1>Coupon</h1>
+				</div>
+			</div>
 		</div>
 	);
 };
