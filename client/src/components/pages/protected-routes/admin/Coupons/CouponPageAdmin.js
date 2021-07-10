89ad6a3dbc8dff5f43cf4e766e5e579a6b81ba56
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-10 20:21:42 +0530

    docs : created dummy component  : coupon.js

---
+++ client/src/components/pages/protected-routes/admin/Coupons/CouponPageAdmin.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {} from "../../../../../functions/coupons";
+
+const CouponPageAdmin = () => {
+	return (
+		<div className="container">
+			<h1>Coupon</h1>
+		</div>
+	);
+};
+
+export default CouponPageAdmin;
