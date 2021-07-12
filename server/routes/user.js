db01f496aa7741bf505a2716264c1ec7c4bdd0f3
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-12 21:22:05 +0530

    update : server/routes/user = added a new endPoint to apply coupon to cart
    - imported and used a controller function , yet to define

--- server/routes/user.js
+++ server/routes/user.js
@@ -13,11 +13,19 @@ const {
 	getUserCart,
 	emptyUserCart,
 	userAddress,
+	applyCouponDiscountToCart,
 } = require("../controllers/userCart");
 
 router.post("/user/cart", createOrUpdateUserMiddleware, userCart); //save cartItems in DB
 router.get("/user/cart", createOrUpdateUserMiddleware, getUserCart); //get user cart info from out own backend
 router.delete("/user/cart", createOrUpdateUserMiddleware, emptyUserCart); // empty cart
 router.post("/user/address", createOrUpdateUserMiddleware, userAddress); //save address of user
+
+//coupon
+router.post(
+	"/user/cart/coupon",
+	createOrUpdateUserMiddleware,
+	applyCouponDiscountToCart
+);
 
 module.exports = router;
