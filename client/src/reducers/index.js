edac0d0346d361d08a7f10775be7974e6114565d
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-13 20:59:18 +0530

    update : reducers/index.js
    - imported couponreducer and combined it with other reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,10 +3,12 @@ import { userReducer } from "./userReducer";
 import { searchReducer } from "./search";
 import { cartReducer } from "./cartReducer";
 import { drawerReducer } from "./drawerReducer";
+import { couponReducer } from "./couponReducer";
 
 export const rootReducer = combineReducers({
 	user: userReducer,
 	search: searchReducer,
 	cart: cartReducer,
 	drawer: drawerReducer,
+	coupon: couponReducer,
 });
