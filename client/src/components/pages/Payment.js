20e7c7e6324894c1e413b616aac1276468218bd4
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-13 21:57:59 +0530

    docs : created dummy component : payment.js

---
+++ client/src/components/pages/Payment.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Payment = () => {
+	return (
+		<div className="container-fluid">
+			<div className="row">
+				<div className="col-md-4">
+					<h3>Payment</h3>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Payment;
