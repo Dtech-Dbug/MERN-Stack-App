577d7fd0bc955811e5391880b8eba3b9b9be85a4
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-15 00:33:56 +0530

    fix error :
    - corrected request URL
    
    - missing body was causing issue
    
    - aded body - empty object

--- client/src/functions/stripe.js
+++ client/src/functions/stripe.js
@@ -1,9 +1,13 @@
 import axios from "axios";
 
-export const createPaymentIntent = (authtoken) => {
-	axios.post("http://loacalhost:8000/api/create-payment-intent", {
-		headers: {
-			authtoken,
-		},
-	});
+export const createPaymentIntent = async (authtoken) => {
+	return await axios.post(
+		"http://localhost:8000/api/create-payment-intent",
+		{},
+		{
+			headers: {
+				authtoken,
+			},
+		}
+	);
 };
