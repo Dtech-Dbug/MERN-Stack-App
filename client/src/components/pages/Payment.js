24978e03706c52d7fe10c8b0e59518c9136ab19a
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-14 12:09:56 +0530

    update : payment.js
    - created a dummy payment component
    
    - brought in loadStripe from @stripe/stripe-js
    
    - brought in Elements frpm @stripe/react-stripe-js

--- client/src/components/pages/Payment.js
+++ client/src/components/pages/Payment.js
@@ -1,13 +1,19 @@
 import React from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+//load strip outside component to be sure it doesnt recreate on every render
+const promise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHER_KEY);
 
 const Payment = () => {
 	return (
-		<div className="container-fluid">
-			<div className="row">
-				<div className="col-md-4">
+		<div className="container p5 text-center">
+			<h4>Continue Your Purchase</h4>
+			<Elements stripe={promise}>
+				<div className="col-md-8 offset-md-2">
 					<h3>Payment</h3>
 				</div>
-			</div>
+			</Elements>
 		</div>
 	);
 };
