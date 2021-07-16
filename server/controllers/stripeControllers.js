e46e4a7e5031ece5082854d483993174e312bec3
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-16 12:13:08 +0530

    fix : stripeController : clientSecret was not being generatead
    - error : invalid object
    
    - fix : correct way of address in shipping was another object acc to stripe params
    
    shiping : {
     name :  '' ,
    address : {
    city : '',
    etc...
    }
    }

--- server/controllers/stripeControllers.js
+++ server/controllers/stripeControllers.js
@@ -5,32 +5,52 @@ const CouponModel = require("../model/couponModel");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 exports.createPaymentIntent = async (req, res) => {
-	console.log("Requet to stripe backend coming");
-	//apply coupon
+	try {
+		console.log(
+			"Requet to stripe backend coming",
+			process.env.STRIPE_SECRET_KEY
+		);
+		//apply coupon
 
-	//later caluculate price
+		//later caluculate price
 
-	const paymentIntent = await stripe.paymentIntents.create({
-		amount: 200,
-		currency: "usd",
-		//shipping & description needed for indian usage
-		//get name and address from userrModel
-		description: "Software development services | Tes",
-		shipping: {
-			name: "Jenny Rosen",
-			address: {
-				line1: "510 Townsend St",
-				postal_code: "98140",
-				city: "San Francisco",
-				state: "CA",
-				country: "US",
+		//1. find the user
+		const user = await User.findOne({ email: req.user.email }).exec();
+
+		//2. Find the user's cart : cartTotal
+		const { cartTotal } = await CartModel.findOne({ orderedBy: user._id });
+
+		const details = {
+			User: user.name,
+			Adress: user.address,
+			Total: cartTotal,
+		};
+		console.table(details);
+
+		//3. calcluate the price to pay : later
+
+		//storing the payment intent
+		//encapsulates details about the transition
+		//uses web hook events to check the statts of paymentIntent,
+		//sends the webhook event instead of the whole paymmentIntent to strip to track success status of payment
+
+		const paymentIntent = await stripe.paymentIntents.create({
+			amount: 100,
+			currency: "usd",
+			description: "TESTING MERN APP",
+			shipping: {
+				name: user.name,
+				address: {
+					line1: user.address,
+				},
 			},
-		},
-	});
+		});
+		console.log("Payment Intent", paymentIntent);
 
-	console.log("secret", paymentIntent.client_secret);
-
-	res.send({
-		clientSecret: paymentIntent.client_secret,
-	});
+		res.send({
+			clientSecret: paymentIntent.client_secret,
+		});
+	} catch (err) {
+		console.log("error in strip controller", err, err.message);
+	}
 };
