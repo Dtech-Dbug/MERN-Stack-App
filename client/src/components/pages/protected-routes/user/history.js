2d36bb5426e8f8ae3ed2cba359137844b4737c81
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-17 22:17:37 +0530

    update : components/pages/protected-routes/history.js
    - created and tested the API from frontend. Working . Responding.

--- client/src/components/pages/protected-routes/user/history.js
+++ client/src/components/pages/protected-routes/user/history.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import UserNav from "../../../Nav/userNav";
+import axios from "axios";
+import { useSelector } from "react-redux";
 
 //we will have to protect this route , so only logged in users can see this page. if the route is not proteted anyone can navigate to this page without even logging
 
 export const History = () => {
+	const [orders, setOrders] = useState("");
+	const { user } = useSelector((state) => ({ ...state }));
+
+	const test = async (authtoken) => {
+		return await axios.get(`http://localhost:8000/api/user/orders`, {
+			headers: { authtoken },
+		});
+	};
+
+	function testing() {
+		console.log("wrokin");
+		test(user.token).then((res) => setOrders(res.data));
+	}
 	return (
 		<div className="container-fluid">
 			<div className="row">
 				<div className="col-md-2">
 					<UserNav />
 				</div>
-				<div className="col">user history page</div>
+				<div className="col">
+					user history page
+					<button onClick={testing}>Test</button>
+					{JSON.stringify(orders)}
+				</div>
 			</div>
 		</div>
 	);
