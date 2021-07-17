a57199eaa38a1812f49c3c6d973561dd08802656
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-17 11:00:04 +0530

    update : server/routes/user.js
    - created a new endPoint to create order
    
    - imported and used a controller : yet to define the controller

--- server/routes/user.js
+++ server/routes/user.js
@@ -14,12 +14,17 @@ const {
 	emptyUserCart,
 	userAddress,
 	applyCouponDiscountToCart,
+
+	createOrder,
 } = require("../controllers/userCart");
 
 router.post("/user/cart", createOrUpdateUserMiddleware, userCart); //save cartItems in DB
 router.get("/user/cart", createOrUpdateUserMiddleware, getUserCart); //get user cart info from out own backend
 router.delete("/user/cart", createOrUpdateUserMiddleware, emptyUserCart); // empty cart
 router.post("/user/address", createOrUpdateUserMiddleware, userAddress); //save address of user
+
+//orders
+router.post("/user/order", createOrUpdateUserMiddleware, createOrder);
 
 //coupon
 router.post(
