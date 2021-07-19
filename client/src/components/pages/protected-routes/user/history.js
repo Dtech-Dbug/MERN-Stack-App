8dbb3a7d2b1b66753a470c45585b3d6279136882
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-19 11:04:02 +0530

    update : client/protected-routes/user/hisroty
    - working on user history page

--- client/src/components/pages/protected-routes/user/history.js
+++ client/src/components/pages/protected-routes/user/history.js
@@ -1,34 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import UserNav from "../../../Nav/userNav";
 import axios from "axios";
 import { useSelector } from "react-redux";
+import { userOrders } from "../../../../functions/userCart";
+import { toast } from "react-toastify";
+import { CheckOutlined, CloseOutlined } from "@ant-design/icons";
 
 //we will have to protect this route , so only logged in users can see this page. if the route is not proteted anyone can navigate to this page without even logging
 
 export const History = () => {
-	const [orders, setOrders] = useState("");
+	const [orders, setOrders] = useState([]);
 	const { user } = useSelector((state) => ({ ...state }));
 
-	const test = async (authtoken) => {
-		return await axios.get(`http://localhost:8000/api/user/orders`, {
-			headers: { authtoken },
-		});
-	};
+	useEffect(() => {
+		user &&
+			userOrders(user.token)
+				.then((res) => {
+					console.log("Order of user", JSON.stringify(res.data, null, 4));
+					setOrders(res.data);
+				})
+				.catch((err) => console.log(err.message));
+	}, []);
 
-	function testing() {
-		console.log("wrokin");
-		test(user.token).then((res) => setOrders(res.data));
-	}
 	return (
 		<div className="container-fluid">
 			<div className="row">
 				<div className="col-md-2">
 					<UserNav />
 				</div>
-				<div className="col">
-					user history page
-					<button onClick={testing}>Test</button>
-					{JSON.stringify(orders)}
+				<div className="col-md-10">
+					<h4>{orders.length > 0 ? "Your Orders" : "	No Orders , found"}</h4>
+
+					{orders.length}
 				</div>
 			</div>
 		</div>
