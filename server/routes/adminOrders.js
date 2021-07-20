624da9f165c2c9feafac72a14fa131c815422383
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-20 12:41:11 +0530

    create : new endpoint for admin - order get and update order statuts

---
+++ server/routes/adminOrders.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+
+//middlewares
+
+const {
+	createOrUpdateUserMiddleware,
+	adminCheckMiddleware,
+} = require("../middlewares/authMiddleware");
+
+//controllers
+const {
+	adminOrders,
+	adminOrderStatus,
+} = require("../controllers/adminOrderControllers");
+
+//routes
+
+router.get(
+	"/admin/orders",
+	createOrUpdateUserMiddleware,
+	adminCheckMiddleware,
+	adminOrders
+);
+
+router.put(
+	"admin/order-status",
+	createOrUpdateUserMiddleware,
+	adminCheckMiddleware,
+	adminOrderStatus
+);
+
+module.exports = router;
