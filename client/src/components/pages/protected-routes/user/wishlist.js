1573e9bb3b75b0df049794225c11e52a509bd96e
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-22 21:05:58 +0530

    update : wishlist.js
    - showed the items in wishlist
    
    - added remove feature

--- client/src/components/pages/protected-routes/user/wishlist.js
+++ client/src/components/pages/protected-routes/user/wishlist.js
@@ -2,10 +2,30 @@ import React, { useState, useEffect } from "react";
 import UserNav from "../../../Nav/userNav";
 import axios from "axios";
 import { useSelector } from "react-redux";
-import { userOrders } from "../../../../functions/userCart";
+import { listWishlist, removeWishlist } from "../../../../functions/userCart";
 import { toast } from "react-toastify";
+import { DeleteOutlined } from "@ant-design/icons";
 
 const Wishlist = () => {
+	const { user } = useSelector((state) => ({ ...state }));
+	const [wishlist, setWishlist] = useState([]);
+
+	useEffect(() => {
+		loadAllWishlist();
+	}, []);
+
+	const loadAllWishlist = () =>
+		listWishlist(user.token).then((res) => {
+			console.log(res);
+			setWishlist(res.data.wishlist);
+		});
+
+	const handleRemoveWishlist = (productId) =>
+		removeWishlist(productId, user.token).then((res) => {
+			toast.error("Deleted");
+			loadAllWishlist();
+		});
+
 	return (
 		<div className="container-fluid">
 			<div className="row">
@@ -13,7 +33,19 @@ const Wishlist = () => {
 					<UserNav />
 				</div>
 
-				<div className="col-md-10">Wishlist</div>
+				<div className="col-md-10">
+					Wishlist
+					{wishlist.map((p) => {
+						return (
+							<div className="alert alert-secondary">
+								{p.title}{" "}
+								<span className="btn btn-sm float-right">
+									<DeleteOutlined onClick={() => handleRemoveWishlist(p._id)} />
+								</span>
+							</div>
+						);
+					})}
+				</div>
 			</div>
 		</div>
 	);
